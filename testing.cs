7044f92114bdb34eee109e0b94f372372c3618c9
Jim Palowski <palowskijim@gmail.com>
2018-03-09 22:45:00 -0800

    Testing

---
+++ testing.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            Console.WriteLine(number);
+        }
+    }
+}
